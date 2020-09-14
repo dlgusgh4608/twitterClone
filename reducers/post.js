ad71e1e8fee56eb6a70715143bc4ca4d14fbc059
dlgusgh4608 <dlgusgh4008@gmail.com>
2020-09-14 17:01:48 +0900

    react-slick error

--- reducers/post.js
+++ reducers/post.js
@@ -7,11 +7,11 @@ export const initialState = {
         },
         content:'my first content #hashtag #express',
         Images: [{
-            src:'https://www.google.com/search?q=%EA%B3%A0%EC%96%91%EC%9D%B4&source=lnms&tbm=isch&sa=X&ved=2ahUKEwj_ztX2suHrAhXy-GEKHQToCEAQ_AUoAXoECBoQAw&biw=1848&bih=981#imgrc=-j0Sy0oFOQWa9M'
+            src:'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Ft1.daumcdn.net%2Fcfile%2Ftistory%2F997C9A4F5D6CA40F2E'
         }, {
-            src:'https://www.google.com/search?q=%EA%B0%95%EC%95%84%EC%A7%80&tbm=isch&ved=2ahUKEwiH8tr3suHrAhWPB94KHf65DXoQ2-cCegQIABAA&oq=%EA%B0%95%EC%95%84%EC%A7%80&gs_lcp=CgNpbWcQAzIFCAAQsQMyBQgAELEDMgUIABCxAzIFCAAQsQMyAggAMgIIADICCAAyAggAMgIIADICCAA6CAgAELEDEIMBUNu0AVjCxAFg0cQBaARwAHgCgAHrAYgB1AeSAQUxLjUuMZgBAKABAaoBC2d3cy13aXotaW1nsAEAwAEB&sclient=img&ei=CpVbX4fFO4-P-Ab-87bQBw&bih=981&biw=1848#imgrc=03GMjyjkGolskM'
+            src:'https://www.sisa-news.com/data/photos/20200730/art_1595497989_752752.jpg'
         }, {
-            src:'https://www.google.com/search?q=%EC%9D%B4%EB%82%98%EC%9D%80&tbm=isch&ved=2ahUKEwjujaChs-HrAhUhNaYKHZ-YAS8Q2-cCegQIABAA&oq=%EC%9D%B4%EB%82%98%EC%9D%80&gs_lcp=CgNpbWcQAzIFCAAQsQMyAggAMgUIABCxAzICCAAyAggAMgIIADICCAAyAggAMgIIADIFCAAQsQM6CAgAELEDEIMBUIwRWI4WYJIWaAJwAHgEgAGqAYgBjAeSAQMyLjaYAQCgAQGqAQtnd3Mtd2l6LWltZ7ABAMABAQ&sclient=img&ei=YpVbX-7UBaHqmAWfsYb4Ag&bih=981&biw=1848#imgrc=UGg0Vnha1XjIHM'
+            src:'https://i.pinimg.com/originals/c8/14/99/c814995b86a60232c93492f5c90c0570.jpg'
         }],
         Comments: [{
             User: {
